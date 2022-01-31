61718d188cdfcdb765efb980e3a89b1e55d70787
SeungHyun <fm10033@gmail.com>
2022-02-01 00:01:04 +0900

    2진법 변환기 완료

--- degitalSystem/changeNotation.js
+++ degitalSystem/changeNotation.js
@@ -1,3 +1,20 @@
-function dec2bin(dicimal) {}
+function dec2bin(dicimal) {
+  let num = dicimal;
+  let answer = [];
+  let last;
+  while (true) {
+    if (num === 1) {
+      answer.push(num);
+      break;
+    }
+    last = Math.floor(num % 2);
+    num = Math.floor(num / 2);
+    console.log(num, last);
+    answer.push(last);
+  }
+  return answer;
+}
 
 function bin2dec(bin) {}
+
+console.log(dec2bin(6));
